1e7e9808c7f7768d8254be90b23e1b0546433446
Brave Leuterio <you@example.com>
2020-10-06 22:24:17 +0800

    almost finished accodion(faqs)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AccordionContainer from './containers/AccordionContainer';
 import FooterContainer from './containers/FooterContainer';
 import JumbotronContainer from './containers/JumbotronContainer';
 
 export default function App() {
   return (
     <>
+      <AccordionContainer />
       <JumbotronContainer />
       <FooterContainer />
     </>
---
+++ src/containers/AccordionContainer.js
@@ -0,0 +1,15 @@
+import faqsData from '../fixtures/faqs.json';
+import { Accordion } from '../components'
+import React from 'react'
+
+export default function AccordionContainer() {
+    return (
+        <Accordion>
+            <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+            {faqsData.map(item => <Accordion.Item key={item.id}>
+                <Accordion.Header>{item.header}</Accordion.Header>
+                <Accordion.Body>{item.body}</Accordion.Body>
+            </Accordion.Item>)}
+        </Accordion>
+    )
+}
