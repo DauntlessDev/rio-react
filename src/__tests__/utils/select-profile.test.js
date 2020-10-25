ed3dd841a4439e6fd327777cb8c98add5e395e4b
Brave Leuterio <you@example.com>
2020-10-25 12:17:11 +0800

    finished utils and profile test

---
+++ src/__tests__/utils/select-profile.test.js
@@ -0,0 +1,48 @@
+import selectionFilter from '../../utils/selectionFilter';
+
+test('selectionFilter with legitimate data', () => {
+    const series = [
+        {
+            id: 'series-1x',
+            title: 'Tiger King',
+            description:
+                'An exploration...',
+            genre: 'documentaries',
+            maturity: '18',
+            slug: 'tiger-king',
+        },
+    ];
+    const films = [
+        {
+            id: 'film-1x',
+            title: 'The Prestige',
+            description: 'Great film...',
+            genre: 'drama',
+            maturity: '15',
+            slug: 'the-prestige',
+        },
+    ];
+
+    const slides = selectionFilter({ series, films });
+    expect(slides.films[0].title).toBe('Drama');
+    expect(slides.films[0].data[0].description).toBe('Great film...');
+    expect(slides.films[0].data[0].genre).toBe('drama');
+    expect(slides.films[0].data[0].maturity).toBe('15');
+    expect(slides.films[0].data[0].slug).toBe('the-prestige');
+
+
+    expect(slides.series[0].title).toBe('Documentaries');
+    expect(slides.series[0].data[0].description).toBe('An exploration...');
+    expect(slides.series[0].data[0].genre).toBe('documentaries');
+    expect(slides.series[0].data[0].maturity).toBe('18');
+    expect(slides.series[0].data[0].slug).toBe('tiger-king');
+});
+
+
+test('selectionFilter with no data', () => {
+    const slides = selectionFilter();
+    expect(slides.series[0].title).toBe('Documentaries');
+    expect(slides.films[0].title).toBe('Drama');
+    expect(slides.series[0].data).toBe(undefined);
+    expect(slides.films[0].data).toBe(undefined);
+});
--- src/containers/SelectProfileContainer.js
+++ src/containers/SelectProfileContainer.js
@@ -17,7 +17,7 @@ export default function SelectProfileContainer({ user, setProfile }) {
             <Profiles>
                 <Profiles.Title>Who's watching?</Profiles.Title>
                 <Profiles.List>
-                    <Profiles.User onClick={() => setProfile({ displayName: user.displayName, photoURL: user.photoURL })}>
+                    <Profiles.User data-testid="user-profile" onClick={() => setProfile({ displayName: user.displayName, photoURL: user.photoURL })}>
                         <Profiles.Picture src={user.photoURL} />
                         <Profiles.Name>{user.displayName}</Profiles.Name>
                     </Profiles.User>
