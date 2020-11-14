9fac6195aea8cf6df78ccb2a18bed9ad4b952c86
Brave Leuterio <you@example.com>
2020-11-14 20:47:26 +0800

    updated header playbutton for video of bunny

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,110 +1,136 @@
-import React, { useState } from 'react';
-import { Link as ReactRouterLink } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
+import { PlayerContext } from "../player";
 import {
-    Background, Container, Feature, Text, Link as TextLink, ButtonLink, Logo, Darken, Inner, Group, FeatureCallOut, FeatureCallOutBottom, Dropdown,
-    SearchIcon,
-    SearchInput, Search, SubText,
-    Picture, Profile, PlayButton,
-} from './styles/header';
+  Background,
+  Container,
+  Feature,
+  Text,
+  Link as TextLink,
+  ButtonLink,
+  Logo,
+  Darken,
+  Inner,
+  Group,
+  FeatureCallOut,
+  FeatureCallOutBottom,
+  Dropdown,
+  SearchIcon,
+  SearchInput,
+  Search,
+  SubText,
+  Picture,
+  Profile,
+  PlayButton,
+} from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
-    return bg ? (
-        <Background {...restProps} data-testid="header-bg">
-            <Darken>
-                {children}
-            </Darken>
-        </Background>
-    ) : <>{children}</>
+  return bg ? (
+    <Background {...restProps} data-testid="header-bg">
+      <Darken>{children}</Darken>
+    </Background>
+  ) : (
+    <>{children}</>
+  );
 }
 
-
 Header.Profile = function HeaderProfile({ children, ...restProps }) {
-    return <Profile {...restProps}>{children}</Profile>;
+  return <Profile {...restProps}>{children}</Profile>;
 };
 
 Header.Picture = function HeaderPicture({ src, ...restProps }) {
-    return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
 };
 
 Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
-    return <Dropdown {...restProps}>{children}</Dropdown>;
+  return <Dropdown {...restProps}>{children}</Dropdown>;
 };
-
 
 Header.Feature = function HeaderFeature({ children, ...restProps }) {
-    return <Feature {...restProps}>{children}</Feature>
-}
+  return <Feature {...restProps}>{children}</Feature>;
+};
 
-Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
-    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
-}
-Header.FeatureCallOutBottom = function HeaderFeatureCallOutBottom({ children, ...restProps }) {
-    return <FeatureCallOutBottom {...restProps}>{children}</FeatureCallOutBottom>
-}
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restProps
+}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+Header.FeatureCallOutBottom = function HeaderFeatureCallOutBottom({
+  children,
+  ...restProps
+}) {
+  return <FeatureCallOutBottom {...restProps}>{children}</FeatureCallOutBottom>;
+};
 
 Header.Text = function HeaderText({ children, ...restProps }) {
-    return <Text {...restProps}>{children}</Text>
-}
+  return <Text {...restProps}>{children}</Text>;
+};
 
 Header.SubText = function HeaderSubText({ children, ...restProps }) {
-    return <SubText {...restProps}>{children}</SubText>
-}
-
+  return <SubText {...restProps}>{children}</SubText>;
+};
 
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
-    return <Container {...restProps}>{children}</Container>
-}
+  return <Container {...restProps}>{children}</Container>;
+};
 
 Header.Group = function HeaderGroup({ children, ...restProps }) {
-    return <Group {...restProps}>{children}</Group>
-}
-
+  return <Group {...restProps}>{children}</Group>;
+};
 
 Header.Inner = function HeaderInner({ children, ...restProps }) {
-    return <Inner {...restProps}>{children}</Inner>
-}
-
-
+  return <Inner {...restProps}>{children}</Inner>;
+};
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
-    return <ButtonLink {...restProps}>{children}</ButtonLink>
-}
-
-
+  return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
 
 Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
-    return <TextLink {...restProps}>{children}</TextLink>
-}
-
-
+  return <TextLink {...restProps}>{children}</TextLink>;
+};
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
-    return (
-        <ReactRouterLink to={to}><Logo {...restProps}></Logo></ReactRouterLink>
-    )
-}
-
-Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps }) {
-    const [searchActive, setSearchActive] = useState(false);
-
-    return (
-        <Search {...restProps}>
-            <SearchIcon onClick={() => setSearchActive((searchActive) => !searchActive)} data-testid="search-click">
-                <img src="/images/icons/search.png" alt="Search" />
-            </SearchIcon>
-            <SearchInput
-                value={searchTerm}
-                onChange={({ target }) => setSearchTerm(target.value)}
-                placeholder="Search films and series"
-                active={searchActive}
-                data-testid="search-input"
-            />
-        </Search>
-    );
+  return (
+    <ReactRouterLink to={to}>
+      <Logo {...restProps}></Logo>
+    </ReactRouterLink>
+  );
 };
 
+Header.Search = function HeaderSearch({
+  searchTerm,
+  setSearchTerm,
+  ...restProps
+}) {
+  const [searchActive, setSearchActive] = useState(false);
+
+  return (
+    <Search {...restProps}>
+      <SearchIcon
+        onClick={() => setSearchActive((searchActive) => !searchActive)}
+        data-testid="search-click"
+      >
+        <img src="/images/icons/search.png" alt="Search" />
+      </SearchIcon>
+      <SearchInput
+        value={searchTerm}
+        onChange={({ target }) => setSearchTerm(target.value)}
+        placeholder="Search films and series"
+        active={searchActive}
+        data-testid="search-input"
+      />
+    </Search>
+  );
+};
 
 Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
-    return <PlayButton {...restProps}>{children}</PlayButton>;
-};
+  const { showPlayer, setShowPlayer } = useContext(PlayerContext);
 
+  return (
+    <PlayButton onClick={() => setShowPlayer((showPlayer) => !showPlayer)}>
+      {children}
+    </PlayButton>
+  );
+};
--- src/containers/BrowseContainer.js
+++ src/containers/BrowseContainer.js
@@ -1,112 +1,136 @@
-import React, { useContext, useEffect, useState } from 'react'
-import Fuse from 'fuse.js'
-import SelectProfileContainer from './SelectProfileContainer'
-import FooterContainer from './FooterContainer'
-import { FirebaseContext } from '../context/firebaseContext'
-import { Card, Header, Loading, Player, } from '../components'
-import * as ROUTES from '../constants/routes'
-import logo from '../logo.png'
+import React, { useContext, useEffect, useState } from "react";
+import Fuse from "fuse.js";
+import SelectProfileContainer from "./SelectProfileContainer";
+import FooterContainer from "./FooterContainer";
+import { FirebaseContext } from "../context/firebaseContext";
+import { Card, Header, Loading, Player } from "../components";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.png";
 
 export default function BrowseContainer({ slides }) {
-    const [category, setCategory] = useState('series')
-    const [searchTerm, setSearchTerm] = useState('')
-    const [profile, setProfile] = useState({})
-    const [loading, setLoading] = useState(true)
-    const { firebase } = useContext(FirebaseContext)
-    const user = firebase.auth().currentUser || {};
-    const [slideRows, setSlideRows] = useState([])
+  const [category, setCategory] = useState("series");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { firebase } = useContext(FirebaseContext);
+  const user = firebase.auth().currentUser || {};
+  const [slideRows, setSlideRows] = useState([]);
 
-    useEffect(() => {
-        console.log('profile', profile)
-        setTimeout(() => {
-            setLoading(false)
-        }, 3000)
-    }, [profile])
+  useEffect(() => {
+    console.log("profile", profile);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile]);
 
-    useEffect(() => {
-        setSlideRows(slides[category])
-    }, [slides, category])
+  useEffect(() => {
+    setSlideRows(slides[category]);
+  }, [slides, category]);
 
-    useEffect(() => {
-        const fuse = new Fuse(slideRows, { keys: ['data.description', 'data.title', 'data.genre'] })
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ["data.description", "data.title", "data.genre"],
+    });
 
-        const results = fuse.search(searchTerm).map(({ item }) => item)
+    const results = fuse.search(searchTerm).map(({ item }) => item);
 
-        if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
-            setSlideRows(results)
-        } else {
-            setSlideRows(slides[category])
-        }
-    }, [searchTerm])
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
-    return profile.displayName ?
-        (
-            loading ?
-                <Loading src={user.photoURL} />
-                :
-                <>
-                    <Loading.ReleaseBody />
-                    <Header src="stranger2" dontShowOnSmallViewPort>
-                        <Header.Frame>
-                            <Header.Group>
-                                <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-                                <Header.TextLink active={category === 'series' ? 'true' : 'false'} onClick={() => setCategory('series')}>SERIES</Header.TextLink>
-                                <Header.TextLink active={category === 'films' ? 'true' : 'false'} onClick={() => setCategory('films')}>FILMS</Header.TextLink>
-                            </Header.Group>
-                            <Header.Group>
-                                <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-                                <Header.Profile>
-                                    <Header.Picture src={user.photoURL} />
-                                    <Header.Dropdown>
-                                        <Header.Group>
-                                            <Header.Picture src={user.photoURL} />
-                                            <Header.TextLink>{user.displayName}</Header.TextLink>
-                                        </Header.Group>
-                                        <Header.Group>
-                                            <Header.TextLink onClick={() => firebase.auth().signOut()}>Sign out</Header.TextLink>
-                                        </Header.Group>
-
-                                    </Header.Dropdown>
-                                </Header.Profile>
-                            </Header.Group>
-                        </Header.Frame>
-                        <Header.Feature>
-                            <Header.SubText>Season 3 now available</Header.SubText>
-                            <Header.FeatureCallOut>STRANGER</Header.FeatureCallOut>
-                            <Header.FeatureCallOutBottom>THINGS</Header.FeatureCallOutBottom>
-                            <Header.Text>Set in the 1980s in the fictional town of Hawkins, Indiana, the first season focuses on the investigation into the disappearance of a young boy amid supernatural events occurring around the town, including the appearance of a girl with psychokinetic abilities.
-                            </Header.Text>
-                            <Header.PlayButton>Start Watching</Header.PlayButton>
-                        </Header.Feature>
-                    </Header>
-                    <Card.Group>
-                        {slideRows.map((slideItem) => (
-                            <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
-                                <Card.Title>{slideItem.title}</Card.Title>
-                                <Card.Entities>
-                                    {slideItem.data.map((item) => (
-                                        <Card.Item key={item.docId} item={item}>
-                                            <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
-                                            <Card.Meta>
-                                                <Card.SubTitle>{item.title}</Card.SubTitle>
-                                                <Card.Text>{item.description}</Card.Text>
-                                            </Card.Meta>
-                                        </Card.Item>
-                                    ))}
-                                </Card.Entities>
-                                <Card.Feature category={category}>
-                                    <Player>
-                                        <Player.Button />
-                                        <Player.Video src="/videos/bunny.mp4" />
-                                    </Player>
-                                </Card.Feature>
-                            </Card>
-                        ))}
-                    </Card.Group>
-                    <FooterContainer />
-                </>
-        ) :
-        (
-            < SelectProfileContainer user={user} setProfile={setProfile} />
-        )
+  return profile.displayName ? (
+    loading ? (
+      <Loading src={user.photoURL} />
+    ) : (
+      <>
+        <Loading.ReleaseBody />
+        <Header src="stranger2" dontShowOnSmallViewPort>
+          <Header.Frame>
+            <Header.Group>
+              <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+              <Header.TextLink
+                active={category === "series" ? "true" : "false"}
+                onClick={() => setCategory("series")}
+              >
+                SERIES
+              </Header.TextLink>
+              <Header.TextLink
+                active={category === "films" ? "true" : "false"}
+                onClick={() => setCategory("films")}
+              >
+                FILMS
+              </Header.TextLink>
+            </Header.Group>
+            <Header.Group>
+              <Header.Search
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+              />
+              <Header.Profile>
+                <Header.Picture src={user.photoURL} />
+                <Header.Dropdown>
+                  <Header.Group>
+                    <Header.Picture src={user.photoURL} />
+                    <Header.TextLink>{user.displayName}</Header.TextLink>
+                  </Header.Group>
+                  <Header.Group>
+                    <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                      Sign out
+                    </Header.TextLink>
+                  </Header.Group>
+                </Header.Dropdown>
+              </Header.Profile>
+            </Header.Group>
+          </Header.Frame>
+          <Header.Feature>
+            <Header.SubText>Season 3 now available</Header.SubText>
+            <Header.FeatureCallOut>STRANGER</Header.FeatureCallOut>
+            <Header.FeatureCallOutBottom>THINGS</Header.FeatureCallOutBottom>
+            <Header.Text>
+              Set in the 1980s in the fictional town of Hawkins, Indiana, the
+              first season focuses on the investigation into the disappearance
+              of a young boy amid supernatural events occurring around the town,
+              including the appearance of a girl with psychokinetic abilities.
+            </Header.Text>
+            <Player>
+              <Header.PlayButton>Start Watching</Header.PlayButton>
+              <Player.Video src="/videos/bunny.mp4" />
+            </Player>
+          </Header.Feature>
+        </Header>
+        <Card.Group>
+          {slideRows.map((slideItem) => (
+            <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+              <Card.Title>{slideItem.title}</Card.Title>
+              <Card.Entities>
+                {slideItem.data.map((item) => (
+                  <Card.Item key={item.docId} item={item}>
+                    <Card.Image
+                      src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                    />
+                    <Card.Meta>
+                      <Card.SubTitle>{item.title}</Card.SubTitle>
+                      <Card.Text>{item.description}</Card.Text>
+                    </Card.Meta>
+                  </Card.Item>
+                ))}
+              </Card.Entities>
+              <Card.Feature category={category}>
+                <Player>
+                  <Player.Button />
+                  <Player.Video src="/videos/bunny.mp4" />
+                </Player>
+              </Card.Feature>
+            </Card>
+          ))}
+        </Card.Group>
+        <FooterContainer />
+      </>
+    )
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
